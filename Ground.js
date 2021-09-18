351b1e3e414394db99fed9959def78442f80aa9c
adhyangupta-blip <adhyangupta17@gmail.com>
2021-09-18 14:31:59 +0530

    testing

--- Ground.js
+++ Ground.js
@@ -7,9 +7,10 @@ class Ground {
       this.width = width;
       this.height = height;
       World.add(world, this.body);
+
     }
     display(){
-      var pos =this.body.position;
+       var pos =this.body.position;
       rectMode(CENTER);
       fill("brown");
       rect(pos.x, pos.y, this.width, this.height);
